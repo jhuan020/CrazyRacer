67858d19b92d2756b52b7cf97aa3e45fda03f8a7
Jacky <jhuan020@ucr.edu>
2013-03-29 18:40:16 -0700

    added movement

--- game.js
+++ game.js
@@ -5,6 +5,23 @@ var player = function(x, y, id, params) {
   this.drawn = new Draw().circle({x: this.x, y: this.y, radius: 10});
 }
 
+player.prototype.step = function() {
+  if (keyDown("LEFT")) {
+    this.x -= 5;
+  }
+  if (keyDown("RIGHT")) {
+    this.x += 5;
+  }
+  if (keyDown("UP")) {
+    this.y -= 5;
+  }
+  if (keyDown("DOWN")) {
+    this.y += 5;
+  }
+}
+
+
+
 $(document).ready(function() {
   setCanvas($("canvas"));
   createInstance(320, 240, player);
