374f8c360a6288b047d5329769bb5a7ecd777dc2
Jacky <jhuan020@ucr.edu>
2013-03-29 20:31:16 -0700

    generate food, when the snake touches it, the food will disappear

--- game.js
+++ game.js
@@ -1,9 +1,28 @@
+var food = function (x, y, id, params){
+  this.id = id;
+  this.x = x;
+  this.y = y;
+  this.width = 10;
+  this.height = 10;
+  this.params = params;
+  this.drawn = new Draw().circle({x:this.x, y:this.y, radius:5});
+}
+
+food.prototype.destroy = function() {
+  this.drawn.undraw();
+}
+
 var player = function(x, y, id, params) {
   this.id = id;
   this.x = x;
   this.y = y;
+  this.width = 100;
+  this.height = 100;
+
+  this.params = params;
   this.drawn = new Draw().circle({x: this.x, y: this.y, radius: 10});
 }
+
 
 player.prototype.step = function() {
   if (keyDown("LEFT")) {
@@ -18,12 +37,23 @@ player.prototype.step = function() {
   if (keyDown("DOWN")) {
     this.y += 5;
   }
+  if (collisionObject(this, food)) {
+    this.x = 320;
+    this.y = 440;
+    destroyInstance(food);
+  }
+
+}
+
+player.prototype.drawObj = function() {
+  this.drawn.update({x: this.x, y: this.y});
 }
 
 
 
 $(document).ready(function() {
   setCanvas($("canvas"));
-  createInstance(320, 240, player);
+  createInstance(320, 440, player);
+  createInstance(300, 300, food);
   start();
 });
