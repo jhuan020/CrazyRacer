57c599a1a5040ce5f562bdde0d728860e0f17359
Jacky <jhuan020@ucr.edu>
2013-04-02 13:37:07 -0700

    crazy racer game

--- game.js
+++ game.js
@@ -1,48 +1,62 @@
-var food = function (x, y, id, params){
+var block = function (x, y, id, movement){
   this.id = id;
   this.x = x;
   this.y = y;
-  this.width = 10;
-  this.height = 10;
-  this.params = params;
-  this.drawn = new Draw().circle({x:this.x, y:this.y, radius:5});
+  this.width = Math.floor((Math.random()*10)+5);
+  this.height = Math.floor((Math.random()*50)+2);
+  this.movement = movement;
+  //this.params = params;
+  this.drawn = new Draw().rectangle({x:this.x, y:this.y, width:this.width, height:this.height, filled:true});
 }
 
-food.prototype.destroy = function() {
+block.prototype.destroy = function() {
   this.drawn.undraw();
 }
 
+block.prototype.step = function(){
+  this.y +=this.movement;
+  if (this.y > 480){
+      destroyInstance(this);
+      createInstance(Math.floor((Math.random()*640)+1), 1, block,Math.floor((Math.random()*10)+1));
+  }
+}
+
+block.prototype.drawObj = function() {
+  this.drawn.update({x: this.x, y: this.y});
+}
+
+//=====================================PLAYER=========================================================
 var player = function(x, y, id, params) {
   this.id = id;
   this.x = x;
   this.y = y;
-  this.width = 100;
-  this.height = 100;
+  this.width = 30;
+  this.height = 30;
 
   this.params = params;
-  this.drawn = new Draw().circle({x: this.x, y: this.y, radius: 10});
+  this.drawn = new Draw().rectangle({x:this.x, y:this.y, width:this.width, height:this.height});
 }
 
 
 player.prototype.step = function() {
   if (keyDown("LEFT")) {
-    this.x -= 5;
+    this.x -= 10;
   }
-  if (keyDown("RIGHT")) {
-    this.x += 5;
+   if (keyDown("RIGHT")) {
+    this.x += 10;
   }
-  if (keyDown("UP")) {
-    this.y -= 5;
+  /*
+   if (keyDown("UP")) {
+    this.y -= 10;
   }
-  if (keyDown("DOWN")) {
-    this.y += 5;
-  }
-  if (collisionObject(this, food)) {
-    this.x = 320;
-    this.y = 440;
-    destroyInstance(food);
-  }
+   if (keyDown("DOWN")) {
+    this.y += 10;
+  }*/
 
+  if (collisionObject(this, block)) {
+    destroyInstance(block);
+    createInstance(320,240,gameover);
+  }
 }
 
 player.prototype.drawObj = function() {
@@ -50,10 +64,24 @@ player.prototype.drawObj = function() {
 }
 
 
+var gameover = function(x, y, id, params) {
+  this.x = x;
+  this.y = y;
+  this.drawn = new Draw().text({x: this.x, y: this.y, text: "Game Over",
+    font: "72pt Helvetica", centered: true});
+}
 
 $(document).ready(function() {
   setCanvas($("canvas"));
   createInstance(320, 440, player);
-  createInstance(300, 300, food);
+  createInstance(10, Math.floor((Math.random()*300)+1), block,Math.floor((Math.random()*10)+1));
+  createInstance(100, Math.floor((Math.random()*300)+1), block,Math.floor((Math.random()*10)+1));
+  createInstance(200, Math.floor((Math.random()*300)+1), block,Math.floor((Math.random()*10)+1));
+  createInstance(300, Math.floor((Math.random()*300)+1), block,Math.floor((Math.random()*10)+1));
+  createInstance(400, Math.floor((Math.random()*300)+1), block,Math.floor((Math.random()*10)+1));
+  createInstance(500, Math.floor((Math.random()*300)+1), block,Math.floor((Math.random()*10)+1));
+  createInstance(600, Math.floor((Math.random()*300)+1), block,Math.floor((Math.random()*10)+1));
   start();
+
+
 });
